f185a7d8b2bf4a356311dda9d35662ec66904587
martinagracy28 <martinagracy28@gmail.com>
2021-05-27 17:03:20 +0530

    last commit

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import './App.css';
 import vabi from "./vault.js";
 import React, { useState,useEffect } from "react";
 import web3 from './web3';
+import token from './token.js';
 
 function App() {
   const [tid,setId] = useState([]);
   const [tid1,setId1] = useState([]);
+  const [tid2,setId2] = useState([]);
+const [tid3,setId3] = useState([]);
   
+  useEffect(()=>{bal()},[])
+  const bal = async() => {
+    let account = await web3.eth.getAccounts();
+    setId2(await token.methods.balanceOf(account[0]).call());
+    setId3(await token.methods._allowances(account[0],"0xccfEfc4a0A0AC9Bd647C536c47423E3F8B560c2d").call());
+  }
   const myfunct = async() => {
    var a = document.getElementById("vlt").value;
    setId(a);
-  //  alert(a);
-   //var d = a * 1000000000;
-   var b = a * 0.00001;
+
+  var b = a/100000;
+ 
   setId1(document.getElementById("print").innerHTML = b);
   
 
@@ -21,8 +30,12 @@ function App() {
   const swap = async() => {
     let account = await web3.eth.getAccounts();
        let amount = tid * 1000000000;
-    let amount1= amount + "000000000";
-    await vabi.methods.transfer("0xcDD7a614895441D1d225D6a81355950c32dcCB7d",amount1).send({from:account[0]});
+    
+    await vabi.methods.tokenFallback(account[0],amount).send({from:account[0]});
+  }
+ const approve = async() => {
+    let account = await web3.eth.getAccounts();
+    await token.methods.approve("0xccfEfc4a0A0AC9Bd647C536c47423E3F8B560c2d",1000000000000000).send({from:account[0]});
   }
   return (
     <div className="App">
@@ -34,17 +47,38 @@ function App() {
        
         <div class="card form-group">
         <h1>BURN VAULT</h1>
-        <text><b>VAULT:  </b></text> <input type = "number" class="form-control" onChange = {myfunct} name="tid" id = "vlt" />
+<br />
+         <text><b>Balance:{tid2/1000000000}</b></text>
+<br />
+<br />
+<div>         
+
+{tid3 <= 0 ? 
+(
+(
+<div>
+<button class="btn btn-primary" onClick={approve}>Approve</button>
+</div>
+)
+):
+(
+(
+<div>
+ <text><b>VAULT:  </b></text> <input type = "number" class="form-control" onChange = {myfunct} name="tid" id = "vlt" />
      
    <br/>
       <br />
       <br />
-    
-      {/* <text><b>BNB: </b>  </text><input type = "number" id ="print"  placeholder = {tid1} ></input> */}
-      <text ><b>BNB: </b>  </text>"<input type = "number" id ="print"  color = "#ff0000"  placeholder = {tid1} ></input>
+
+      <text ><b>BNB: </b>  </text><input type = "number" id ="print"   placeholder = {tid1} ></input>
       <br />
       <br />
       <button onClick = {swap} class="btn">Swap</button>
+</div>
+)
+)}
+    </div> 
+       
      
         </div>
      
