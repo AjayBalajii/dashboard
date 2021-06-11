c16af5bc4ce399a2d24a767fda7f64c41eef05c5
martinagracy28 <martinagracy28@gmail.com>
2021-06-11 16:53:22 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -30,10 +30,9 @@ function App() {
      <Navbar.Brand href="#">
  
    
-<<<<<<< HEAD
-   <Link class="navlink"  exact to="/Black">
-=======
-<<<<<<< HEAD
+
+   
+
    <Link class="navlink"  exact to="/Black">
 
 BlackCollateral Page 
@@ -45,24 +44,9 @@ Burn Vault{' '}
        </Link>
 
  
-=======
-   <Link class="navlink"  exact to="/h">
->>>>>>> 0c06b04d5b3dc07581e0920a6947ce8979a7d976
 
-BlackCollateral Page 
-
-</Link>
  
- <Link class="navlink"  exact to="/vault">
-Burn Vault{' '}
-       </Link>
 
-<<<<<<< HEAD
- 
-=======
-
->>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
->>>>>>> 0c06b04d5b3dc07581e0920a6947ce8979a7d976
 </Navbar.Brand>
 <Navbar.Collapse className="justify-content-end">
 
@@ -70,36 +54,19 @@ Burn Vault{' '}
  </Navbar.Collapse>
  </Navbar>
      <Switch>
-<<<<<<< HEAD
+
 <Route exact path='/Black' component={Black}/>
 <Route exact path='/vault' component={Vault}/>
-=======
-<<<<<<< HEAD
-<Route exact path='/Black' component={Black}/>
-<Route exact path='/vault' component={Vault}/>
-=======
-<Route exact path='/h' component={Black}/>
-<Route exact path='/t' component={Vault}/>
->>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
->>>>>>> 0c06b04d5b3dc07581e0920a6947ce8979a7d976
- 
+
 </Switch>
 
 </Router>
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0c06b04d5b3dc07581e0920a6947ce8979a7d976
 
 
 
 
-<<<<<<< HEAD
-=======
-=======
->>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
->>>>>>> 0c06b04d5b3dc07581e0920a6947ce8979a7d976
+
 </div>
 </div>
   );
