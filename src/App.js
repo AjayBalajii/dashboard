d0b3958ac94c7a1d98f0d541efc702c3d26f5bf4
martinagracy28 <martinagracy28@gmail.com>
2021-06-11 16:58:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -21,8 +21,13 @@ function App() {
   return (
      
    <div class="bg-dark">
+<<<<<<< HEAD
  
+=======
+>>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
         
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
+integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous"/>
 
    <div>
    <Router>
@@ -30,9 +35,7 @@ function App() {
      <Navbar.Brand href="#">
  
    
-
-   
-
+<<<<<<< HEAD
    <Link class="navlink"  exact to="/Black">
 
 BlackCollateral Page 
@@ -44,9 +47,19 @@ Burn Vault{' '}
        </Link>
 
  
+=======
+   <Link class="navlink"  exact to="/h">
 
+Deploy 
+
+</Link>
  
+ <Link class="navlink"  exact to="/t">
+Vault{' '}
+       </Link>
 
+
+>>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
 </Navbar.Brand>
 <Navbar.Collapse className="justify-content-end">
 
@@ -54,19 +67,25 @@ Burn Vault{' '}
  </Navbar.Collapse>
  </Navbar>
      <Switch>
-
+<<<<<<< HEAD
 <Route exact path='/Black' component={Black}/>
 <Route exact path='/vault' component={Vault}/>
-
+=======
+<Route exact path='/h' component={Black}/>
+<Route exact path='/t' component={Vault}/>
+>>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
+ 
 </Switch>
 
 </Router>
 
+<<<<<<< HEAD
 
 
 
 
-
+=======
+>>>>>>> cef8ab11bb8f43455da9f6419b0fdb4c4b320694
 </div>
 </div>
   );
